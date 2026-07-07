f9328d90aa4dd75c3ddb3ecc25058e4161fbcd29
patelgyts321-sys <patelgyts321@gmail.com>
2026-07-07 08:22:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from telegram import Update
+from telegram.ext import Application, MessageHandler, ContextTypes, filters
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def auto_leave(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.effective_chat.type in ["group", "supergroup"]:
+        await context.bot.leave_chat(update.effective_chat.id)
+
+app = Application.builder().token(BOT_TOKEN).build()
+app.add_handler(MessageHandler(filters.ALL, auto_leave))
+
+app.run_polling()
